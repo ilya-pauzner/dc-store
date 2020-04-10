e10f42b788f81c345c7e53b1080945a2942167b6
ilya-pauzner <ilya-pauzner@yandex.ru>
2020-04-11 01:09:04 +0300

    Split handlers into smaller (but still large) functions.

--- main.go
+++ main.go
@@ -38,78 +38,85 @@ func main() {
 	}
 
 	// Create Stock, Get All Stocks
-	http.HandleFunc("/stocks", handler1)
+	http.HandleFunc("/stocks", stocksHandler)
 
 	// Get Stock, Modify Stock, Delete Stock
-	http.HandleFunc("/stocks/", handler2)
+	http.HandleFunc("/stocks/", stockIdHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handler1(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		// Create Stock
-		contents, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusBadRequest)
-		}
-
-		var stock Stock
-		err = json.Unmarshal(contents, &stock)
-		if err != nil {
-			http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
-		}
-
-		stock.Code = rand.Uint64()
-
-		contents, err = json.Marshal(stock)
-		if err != nil {
-			http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
-		}
-
-		_, err = client.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
-		if err != nil {
-			http.Error(w, "Failed to update database", http.StatusInternalServerError)
-		}
-
-		_, _ = w.Write(contents)
-
-	} else {
-		if r.Method == "" || r.Method == "GET" {
-			// Get All Stocks
-			keys, err := client.Keys("*").Result()
-			if err != nil {
-				http.Error(w, "Failed to get keys from database", http.StatusInternalServerError)
-			}
-			stocks := make([]Stock, len(keys))
-			for i, key := range keys {
-				contents, err := client.Get(key).Result()
-				if err != nil {
-					http.Error(w, "Failed to get from database", http.StatusInternalServerError)
-				}
-				if contents == "" {
-					http.Error(w, "No such stock", http.StatusNotFound)
-				}
-
-				var stock Stock
-				er := json.Unmarshal([]byte(contents), &stock)
-				if er != nil {
-					http.Error(w, "Failed to unmarshal data from database", http.StatusInternalServerError)
-				}
-				stocks[i] = stock
-			}
-			contents, er := json.Marshal(stocks)
-			if er != nil {
-				http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
-			}
-
-			_, _ = w.Write(contents)
-		} else {
-			http.Error(w, "Unknown method", http.StatusBadRequest)
-		}
+func stocksHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		CreateStock(w, r)
+	case "", "GET":
+		GetAllStocks(w, r)
+	default:
+		http.Error(w, "Unknown method", http.StatusBadRequest)
 	}
 }
 
-func handler2(w http.ResponseWriter, r *http.Request) {
+func CreateStock(w http.ResponseWriter, r *http.Request) {
+	contents, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+	}
+
+	var stock Stock
+	err = json.Unmarshal(contents, &stock)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
+	}
+
+	stock.Code = rand.Uint64()
+
+	contents, err = json.Marshal(stock)
+	if err != nil {
+		http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
+	}
+
+	_, err = client.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
+	if err != nil {
+		http.Error(w, "Failed to update database", http.StatusInternalServerError)
+	}
+
+	_, _ = w.Write(contents)
+}
+
+func GetAllStocks(w http.ResponseWriter, r *http.Request) {
+	keys, err := client.Keys("*").Result()
+	if err != nil {
+		http.Error(w, "Failed to get keys from database", http.StatusInternalServerError)
+	}
+
+	stocks := make([]Stock, len(keys))
+	for i, key := range keys {
+		contents, err := client.Get(key).Result()
+		if err != nil {
+			http.Error(w, "Failed to get from database", http.StatusInternalServerError)
+		}
+		if contents == "" {
+			http.Error(w, "No such stock", http.StatusNotFound)
+		}
+
+		var stock Stock
+		er := json.Unmarshal([]byte(contents), &stock)
+		if er != nil {
+			http.Error(w, "Failed to unmarshal data from database", http.StatusInternalServerError)
+		}
+
+		stocks[i] = stock
+	}
+
+	contents, er := json.Marshal(stocks)
+	if er != nil {
+		http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
+	}
+
+	_, _ = w.Write(contents)
+}
+
+func stockIdHandler(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
 	key := parts[len(parts)-1]
 
@@ -118,63 +125,68 @@ func handler2(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad stock number", http.StatusBadRequest)
 	}
 
-	if r.Method == "PUT" {
-		// Modify Stock
-		contents, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusBadRequest)
-		}
+	switch r.Method {
+	case "PUT":
+		ModifyStock(w, r, code)
+	case "", "GET":
+		GetStock(w, r, code)
+	case "DELETE":
+		DeleteStock(w, r, code)
+	default:
+		http.Error(w, "Unknown method", http.StatusBadRequest)
+	}
+}
 
-		var stock Stock
-		err = json.Unmarshal(contents, &stock)
-		if err != nil {
-			http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
-		}
+func ModifyStock(w http.ResponseWriter, r *http.Request, code uint64) {
+	contents, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+	}
 
-		stock.Code = code
+	var stock Stock
+	err = json.Unmarshal(contents, &stock)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
+	}
 
-		contents, err = json.Marshal(stock)
-		if err != nil {
-			http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
-		}
+	stock.Code = code
 
-		_, err = client.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
-		if err != nil {
-			http.Error(w, "Failed to update database", http.StatusInternalServerError)
-		}
+	contents, err = json.Marshal(stock)
+	if err != nil {
+		http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
+	}
 
-		_, _ = w.Write(contents)
+	_, err = client.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
+	if err != nil {
+		http.Error(w, "Failed to update database", http.StatusInternalServerError)
+	}
 
-	} else {
-		if r.Method == "" || r.Method == "GET" {
-			// Get Stock
-			stock, err := client.Get(strconv.FormatUint(code, 10)).Result()
-			if err != nil {
-				http.Error(w, "Failed to get from database", http.StatusInternalServerError)
-			}
-			if stock == "" {
-				http.Error(w, "No such stock", http.StatusNotFound)
-			}
+	_, _ = w.Write(contents)
+}
 
-			_, _ = w.Write([]byte(stock))
-		} else {
-			if r.Method == "DELETE" {
-				// Delete Stock
-				stock, err := client.Get(strconv.FormatUint(code, 10)).Result()
-				if err != nil {
-					http.Error(w, "Failed to get from database", http.StatusInternalServerError)
-				}
-				if stock == "" {
-					http.Error(w, "No such stock", http.StatusNotFound)
-				}
+func GetStock(w http.ResponseWriter, r *http.Request, code uint64) {
+	stock, err := client.Get(strconv.FormatUint(code, 10)).Result()
+	if err != nil {
+		http.Error(w, "Failed to get from database", http.StatusInternalServerError)
+	}
+	if stock == "" {
+		http.Error(w, "No such stock", http.StatusNotFound)
+	}
 
-				_, err = client.Del(strconv.FormatUint(code, 10)).Result()
-				if err != nil {
-					http.Error(w, "Failed to delete from database", http.StatusInternalServerError)
-				}
-			} else {
-				http.Error(w, "Unknown method", http.StatusBadRequest)
-			}
-		}
+	_, _ = w.Write([]byte(stock))
+}
+
+func DeleteStock(w http.ResponseWriter, r *http.Request, code uint64) {
+	stock, err := client.Get(strconv.FormatUint(code, 10)).Result()
+	if err != nil {
+		http.Error(w, "Failed to get from database", http.StatusInternalServerError)
+	}
+	if stock == "" {
+		http.Error(w, "No such stock", http.StatusNotFound)
+	}
+
+	_, err = client.Del(strconv.FormatUint(code, 10)).Result()
+	if err != nil {
+		http.Error(w, "Failed to delete from database", http.StatusInternalServerError)
 	}
 }
