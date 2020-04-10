dbd6e71b8d1600584db12055be6b1eea62cc9bf6
ilya-pauzner <ilya-pauzner@yandex.ru>
2020-04-11 01:54:39 +0300

    Added register, authorize, refresh, validate functions.

--- main.go
+++ main.go
@@ -3,7 +3,10 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"encoding/json"
+	"errors"
 	"github.com/go-redis/redis/v7"
 	"io/ioutil"
 	"log"
@@ -21,42 +24,241 @@ type Stock struct {
 }
 
 var (
-	client *redis.Client
+	stocksClient        *redis.Client
+	passwordsClient     *redis.Client
+	accessTokensClient  *redis.Client
+	refreshTokensClient *redis.Client
 )
 
 func main() {
-	client = redis.NewClient(&redis.Options{
+	stocksClient = redis.NewClient(&redis.Options{
 		Addr:            "db:6379",
 		Password:        "", // no password set
 		DB:              0,  // use default DB
 		MaxRetries:      3,
 		MaxRetryBackoff: 5 * time.Second,
 	})
-
-	if client == nil {
+	if stocksClient == nil {
 		log.Fatal("Failed to create Redis Client")
 	}
 
-	// Create Stock, Get All Stocks
+	passwordsClient = redis.NewClient(&redis.Options{
+		Addr:            "db:6379",
+		Password:        "", // no password set
+		DB:              1,
+		MaxRetries:      3,
+		MaxRetryBackoff: 5 * time.Second,
+	})
+	if passwordsClient == nil {
+		log.Fatal("Failed to create Redis Client")
+	}
+
+	accessTokensClient = redis.NewClient(&redis.Options{
+		Addr:            "db:6379",
+		Password:        "", // no password set
+		DB:              2,
+		MaxRetries:      3,
+		MaxRetryBackoff: 5 * time.Second,
+	})
+	if accessTokensClient == nil {
+		log.Fatal("Failed to create Redis Client")
+	}
+
+	refreshTokensClient = redis.NewClient(&redis.Options{
+		Addr:            "db:6379",
+		Password:        "", // no password set
+		DB:              3,
+		MaxRetries:      3,
+		MaxRetryBackoff: 5 * time.Second,
+	})
+	if refreshTokensClient == nil {
+		log.Fatal("Failed to create Redis Client")
+	}
+
+	// Will be moved to another service
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/authorize", authorize)
+	http.HandleFunc("/refresh", refresh)
+	http.HandleFunc("/validate", validate)
+
+	// createStock, getAllStocks
 	http.HandleFunc("/stocks", stocksHandler)
 
-	// Get Stock, Modify Stock, Delete Stock
+	// getStock, modifyStock, deleteStock
 	http.HandleFunc("/stocks/", stockIdHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	contents, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+	}
+
+	var data map[string]string
+	err = json.Unmarshal(contents, &data)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
+	}
+
+	login, ok := data["login"]
+	if !ok {
+		http.Error(w, "Failed to get login from request body", http.StatusBadRequest)
+	}
+
+	password, ok := data["password"]
+	if !ok {
+		http.Error(w, "Failed to get password from request body", http.StatusBadRequest)
+	}
+
+	hash := sha256.New()
+	hashedPassword := hash.Sum([]byte(password))
+
+	_, err = passwordsClient.Set(login, hashedPassword, 0).Result()
+	if err != nil {
+		http.Error(w, "Failed to update database", http.StatusInternalServerError)
+	}
+
+	_, _ = w.Write([]byte("Registration successful!"))
+}
+
+func authorize(w http.ResponseWriter, r *http.Request) {
+	contents, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+	}
+
+	var data map[string]string
+	err = json.Unmarshal(contents, &data)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
+	}
+
+	login, ok := data["login"]
+	if !ok {
+		http.Error(w, "Failed to get login from request body", http.StatusBadRequest)
+	}
+
+	password, ok := data["password"]
+	if !ok {
+		http.Error(w, "Failed to get password from request body", http.StatusBadRequest)
+	}
+
+	hash := sha256.New()
+	hashedPassword := hash.Sum([]byte(password))
+
+	hashedPasswordInDataBase, err := passwordsClient.Get(login).Result()
+	if !bytes.Equal(hashedPassword, []byte(hashedPasswordInDataBase)) {
+		http.Error(w, "Wrong password", http.StatusForbidden)
+	}
+
+	tokens := make(map[string]string)
+
+	refreshToken := rand.Uint64()
+	refreshTokenString := strconv.FormatUint(refreshToken, 10)
+	tokens["refresh_token"] = refreshTokenString
+	_, err = refreshTokensClient.Set(refreshTokenString, login, 0).Result()
+	if err != nil {
+		http.Error(w, "Failed to update database", http.StatusInternalServerError)
+	}
+
+	accessToken := rand.Uint64()
+	accessTokenString := strconv.FormatUint(accessToken, 10)
+	tokens["access_token"] = accessTokenString
+	_, err = accessTokensClient.Set(accessTokenString, refreshTokenString, time.Hour).Result()
+	if err != nil {
+		http.Error(w, "Failed to update database", http.StatusInternalServerError)
+	}
+
+	contents, err = json.Marshal(tokens)
+	if err != nil {
+		http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
+	}
+
+	_, _ = w.Write(contents)
+}
+
+func refresh(w http.ResponseWriter, r *http.Request) {
+	contents, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+	}
+
+	var data map[string]string
+	err = json.Unmarshal(contents, &data)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
+	}
+
+	refreshTokenString, ok := data["refresh_token"]
+	if !ok {
+		http.Error(w, "Failed to get refresh_token from request body", http.StatusBadRequest)
+	}
+
+	_, err = refreshTokensClient.Get(refreshTokenString).Result()
+	if errors.Is(err, redis.Nil) {
+		http.Error(w, "Wrong refresh_token", http.StatusForbidden)
+	} else if err != nil {
+		http.Error(w, "Failed to get from database", http.StatusInternalServerError)
+	}
+
+	tokens := make(map[string]string)
+
+	accessToken := rand.Uint64()
+	accessTokenString := strconv.FormatUint(accessToken, 10)
+	tokens["access_token"] = accessTokenString
+	_, err = accessTokensClient.Set(accessTokenString, refreshTokenString, time.Hour).Result()
+	if err != nil {
+		http.Error(w, "Failed to update database", http.StatusInternalServerError)
+	}
+
+	contents, err = json.Marshal(tokens)
+	if err != nil {
+		http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
+	}
+
+	_, _ = w.Write(contents)
+}
+
+func validate(w http.ResponseWriter, r *http.Request) {
+	contents, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+	}
+
+	var data map[string]string
+	err = json.Unmarshal(contents, &data)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
+	}
+
+	accessTokenString, ok := data["access_token"]
+	if !ok {
+		http.Error(w, "Failed to get access_token from request body", http.StatusBadRequest)
+	}
+
+	_, err = accessTokensClient.Get(accessTokenString).Result()
+	if errors.Is(err, redis.Nil) {
+		http.Error(w, "Wrong access_token", http.StatusForbidden)
+	} else if err != nil {
+		http.Error(w, "Failed to get from database", http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func stocksHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		CreateStock(w, r)
+		createStock(w, r)
 	case "", "GET":
-		GetAllStocks(w, r)
+		getAllStocks(w, r)
 	default:
 		http.Error(w, "Unknown method", http.StatusBadRequest)
 	}
 }
 
-func CreateStock(w http.ResponseWriter, r *http.Request) {
+func createStock(w http.ResponseWriter, r *http.Request) {
 	contents, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Failed to read request body", http.StatusBadRequest)
@@ -75,7 +277,7 @@ func CreateStock(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
 	}
 
-	_, err = client.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
+	_, err = stocksClient.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
 	if err != nil {
 		http.Error(w, "Failed to update database", http.StatusInternalServerError)
 	}
@@ -83,15 +285,15 @@ func CreateStock(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(contents)
 }
 
-func GetAllStocks(w http.ResponseWriter, r *http.Request) {
-	keys, err := client.Keys("*").Result()
+func getAllStocks(w http.ResponseWriter, r *http.Request) {
+	keys, err := stocksClient.Keys("*").Result()
 	if err != nil {
 		http.Error(w, "Failed to get keys from database", http.StatusInternalServerError)
 	}
 
 	stocks := make([]Stock, len(keys))
 	for i, key := range keys {
-		contents, err := client.Get(key).Result()
+		contents, err := stocksClient.Get(key).Result()
 		if err != nil {
 			http.Error(w, "Failed to get from database", http.StatusInternalServerError)
 		}
@@ -127,17 +329,17 @@ func stockIdHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "PUT":
-		ModifyStock(w, r, code)
+		modifyStock(w, r, code)
 	case "", "GET":
-		GetStock(w, r, code)
+		getStock(w, r, code)
 	case "DELETE":
-		DeleteStock(w, r, code)
+		deleteStock(w, r, code)
 	default:
 		http.Error(w, "Unknown method", http.StatusBadRequest)
 	}
 }
 
-func ModifyStock(w http.ResponseWriter, r *http.Request, code uint64) {
+func modifyStock(w http.ResponseWriter, r *http.Request, code uint64) {
 	contents, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Failed to read request body", http.StatusBadRequest)
@@ -156,7 +358,7 @@ func ModifyStock(w http.ResponseWriter, r *http.Request, code uint64) {
 		http.Error(w, "Failed to marshal response body", http.StatusInternalServerError)
 	}
 
-	_, err = client.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
+	_, err = stocksClient.Set(strconv.FormatUint(stock.Code, 10), contents, 0).Result()
 	if err != nil {
 		http.Error(w, "Failed to update database", http.StatusInternalServerError)
 	}
@@ -164,8 +366,8 @@ func ModifyStock(w http.ResponseWriter, r *http.Request, code uint64) {
 	_, _ = w.Write(contents)
 }
 
-func GetStock(w http.ResponseWriter, r *http.Request, code uint64) {
-	stock, err := client.Get(strconv.FormatUint(code, 10)).Result()
+func getStock(w http.ResponseWriter, r *http.Request, code uint64) {
+	stock, err := stocksClient.Get(strconv.FormatUint(code, 10)).Result()
 	if err != nil {
 		http.Error(w, "Failed to get from database", http.StatusInternalServerError)
 	}
@@ -176,8 +378,8 @@ func GetStock(w http.ResponseWriter, r *http.Request, code uint64) {
 	_, _ = w.Write([]byte(stock))
 }
 
-func DeleteStock(w http.ResponseWriter, r *http.Request, code uint64) {
-	stock, err := client.Get(strconv.FormatUint(code, 10)).Result()
+func deleteStock(w http.ResponseWriter, r *http.Request, code uint64) {
+	stock, err := stocksClient.Get(strconv.FormatUint(code, 10)).Result()
 	if err != nil {
 		http.Error(w, "Failed to get from database", http.StatusInternalServerError)
 	}
@@ -185,7 +387,7 @@ func DeleteStock(w http.ResponseWriter, r *http.Request, code uint64) {
 		http.Error(w, "No such stock", http.StatusNotFound)
 	}
 
-	_, err = client.Del(strconv.FormatUint(code, 10)).Result()
+	_, err = stocksClient.Del(strconv.FormatUint(code, 10)).Result()
 	if err != nil {
 		http.Error(w, "Failed to delete from database", http.StatusInternalServerError)
 	}
